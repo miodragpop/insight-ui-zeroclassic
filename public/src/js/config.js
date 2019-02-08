f40b3c6e07ddde932e7d07fbe8462acdfd28aa15
Miodrag Popović <miodragpopovic@stcable.net>
2019-02-08 19:29:22 +0100

    zeroclassic fixes

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Snowgem Block '
+      title: 'ZeroClassic Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Snowgem Transaction '
+      title: 'ZeroClassic Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Snowgem Blocks solved Today'
+      title: 'ZeroClassic Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Snowgem Blocks solved '
+      title: 'ZeroClassic Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Snowgem Address '
+      title: 'ZeroClassic Address '
     }).
     when('/charts/:chartType?', {
       templateUrl: 'views/charts.html',
